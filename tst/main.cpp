a359bc5772f84499a8a132bc3c2999a877d795eb
furkanpham <furkanpham@users.noreply.github.com>
2022-06-06 22:00:52 +0200

    Remove spdlog dependency

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,11 +1,7 @@
 #include "gtest/gtest.h"
-#include <spdlog/spdlog.h>
 
 int main(int argc, char **argv)
 {
-    spdlog::flush_on(spdlog::level::debug);
-    spdlog::set_level(spdlog::level::trace);
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
