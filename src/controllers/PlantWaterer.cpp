fd4a14dac67c476eaaa6fc7fb276fab1c599f133
furkanpham <furkanpham@users.noreply.github.com>
2022-06-07 09:50:14 +0200

    Add doxygen comments

--- src/controllers/PlantWaterer.cpp
+++ src/controllers/PlantWaterer.cpp
@@ -1,31 +1,52 @@
 #include "PlantWaterer.h"
 
+//!
+//! \brief A PlantWaterer object, used to water plants (or other things you may find worth watering)
+//!
 PlantWaterer::PlantWaterer() :
     mName(""),
     mWaterPercentage(0)
 {
 }
 
+//!
+//! \brief Return name of PlantWaterer
+//! \return name of PlantWaterer, or an empty string if name has not been set
+//!
 std::string PlantWaterer::getName()
 {
     return mName;
 }
 
+//!
+//! \brief Sets a new name for PlantWaterer
+//! \param newName a string used to set new name of PlantWaterer
+//!
 void PlantWaterer::setName(std::string newName)
 {
     mName = newName;
 }
 
+//!
+//! \brief Return the percentage of water left in PlantWaterer
+//! \return percentage of water in PlantWaterer
+//!
 int PlantWaterer::getWaterPercentage()
 {
     return mWaterPercentage;
 }
 
+//!
+//! \brief Fully fills up PlantWaterer
+//!
 void PlantWaterer::fillWater()
 {
     mWaterPercentage = 100;
 }
 
+//!
+//! \brief Use up a small amount of water to spray some water (preferably on plants)
+//!
 void PlantWaterer::sprayWater()
 {
     mWaterPercentage -= 15;
